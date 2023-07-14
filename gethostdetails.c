1103461628be671590e1a52dc15e89fbfe65ff8a
djedd1ne <mekki.djallaleddine@gmail.com>
2023-07-14 13:27:38 +0200

    Get hostname data
    using gethostbyname that returns hostent data
    we use inet_ntoa to convert the addresses to IPv4 numbers and dots

--- gethostdetails.c
+++ gethostdetails.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
 int main()
 {
     char    host[BUFSIZ];
     int     r;
+    char    **addresses;
+    struct  hostent *hostdata;
 
+    /*report the hostname*/
     r = gethostname(host, BUFSIZ);
     if (r == -1)
     {
@@ -14,5 +19,21 @@ int main()
         exit (1);
     }
     printf("This host is named: '%s'\n", host);
+    
+    /*get information about the host*/
+    hostdata = gethostbyname(host);
+    if (hostdata == NULL)
+    {
+        fprintf(stderr, "Can't obtain host data\n");
+        exit(1);
+    }
+    printf("Adress(es): ");
+    addresses = hostdata->h_addr_list;
+    /*convet binary form to IPv4 numbers and dots*/
+    while (*addresses)
+    {
+        printf("%s\n", inet_ntoa(*(struct in_addr *)*addresses));
+        addresses++;
+    }
     return (0);
 }
